1b1abd2d79f756ab266c1b59d72772b6bba548db
JoeTinnySpace <josephdennisjoo71gmail.com>
2024-04-17 00:55:52 +0530

    changed domain names, removed anime

--- Movierulzhd/src/main/kotlin/com/hexated/Extractors.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Extractors.kt
@@ -6,8 +6,8 @@ import com.lagradost.cloudstream3.extractors.Chillx
 import com.lagradost.cloudstream3.utils.*
 
 class Sbnmp : ExtractorApi() {
-    override val name = "Sbnmp"
-    override var mainUrl = "https://sbnmp.bar"
+    override val name = "fmhd"
+    override var mainUrl = "https://fmhd.bar"
     override val requiresReferer = true
 
     override suspend fun getUrl(
@@ -38,35 +38,3 @@ class Sbnmp : ExtractorApi() {
     }
 }
 
-open class Akamaicdn : ExtractorApi() {
-    override val name = "Akamaicdn"
-    override val mainUrl = "https://akamaicdn.life"
-    override val requiresReferer = true
-
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        val res = app.get(url, referer = referer).document
-        val mappers = res.selectFirst("script:containsData(sniff\\()")?.data()?.substringAfter("sniff(")
-            ?.substringBefore(");") ?: return
-        val ids = mappers.split(",").map { it.replace("\"", "") }
-        callback.invoke(
-            ExtractorLink(
-                this.name,
-                this.name,
-                "$mainUrl/m3u8/${ids[1]}/${ids[2]}/master.txt?s=1&cache=1",
-                url,
-                Qualities.Unknown.value,
-                isM3u8 = true,
-            )
-        )
-    }
-}
-
-class AnimesagaStream : Chillx() {
-    override val name = "AnimesagaStream"
-    override val mainUrl = "https://stream.anplay.in"
-}
